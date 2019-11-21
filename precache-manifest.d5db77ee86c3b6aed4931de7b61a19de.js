eadf7d96853479bdf02ccd24834e836535ee6941
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2019-11-21 18:17:36 -0500

    Updates

---
+++ precache-manifest.d5db77ee86c3b6aed4931de7b61a19de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58d45b09c5eb1cdb4ef30270f2bf54c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d68b42edb67f7580bba3",
+    "url": "/Portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "16479721abdafdf23bc8",
+    "url": "/Portfolio/static/js/2.55e1b789.chunk.js"
+  },
+  {
+    "revision": "d68b42edb67f7580bba3",
+    "url": "/Portfolio/static/js/main.4ab0229a.chunk.js"
+  },
+  {
+    "revision": "dd65a4e71a8b70668f2c",
+    "url": "/Portfolio/static/js/runtime-main.6a869909.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
