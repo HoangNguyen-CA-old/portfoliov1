b3c120d8502c04583f574102711ab68ccecff35d
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2020-01-23 20:43:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.047613247b51d03171d40c8b5633294f.js"
+  "/Portfolio/precache-manifest.3b0c4cb71200209327e7468514e58072.js"
 );
 
 self.addEventListener('message', (event) => {
